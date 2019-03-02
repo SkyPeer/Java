6914bce7ba6eec0b11beb054fa450a76062e5265
Oleg <o.selivancev@gmail.com>
2019-03-03 02:04:04 +0300

    Java practiceUpdate

--- HelloWorld/src/com/javalesson/hello/HelloWorld.java
+++ HelloWorld/src/com/javalesson/hello/HelloWorld.java
@@ -6,10 +6,20 @@ public class HelloWorld {
         System.out.println("Hello world!!!");
 
         byte b = 10;
-        short c = 32000; //по таблице переменных от  -32768 до 32767
+        short c = 200; //по таблице переменных от  -32768 до 32767
         int a = b + 23; // int тип переменной
-        //long myLongValue = 456789;
-        long myLongValue = 123456789;
+        long myLongValue = 123_456_789L;
+        float myFloatVal = 23.56f;
+        double myDoubleVal = 56.46d;
+
+            double sum = a+b+c+myLongValue + myDoubleVal + myFloatVal;
+        System.out.println("SUM = " + sum);
+
+        float floatResult = myFloatVal / 3;
+        System.out.println("floatResult = " + floatResult);
+        double myNewDouble = 23.56;
+
+        System.out.println("myDoubleResult = " + myNewDouble / 3);
 
         System.out.println("byte b =" +b);
         System.out.println("short c =" +c);
