3fda7ee1a3dd6cf46e81cc2b6d9c988456bbaa74
Oleg <o.selivancev@gmail.com>
2019-03-10 02:46:50 +0300

    JavaLessonUpdate

---
+++ HelloWorld/src/com/javalesson/operators/Operators.java
@@ -0,0 +1,27 @@
+package com.javalesson.operators;
+
+public class Operators {
+
+    public static void main(String[] args) {
+//          + - / *  % - остаток от деления
+        int a = 10;
+        int b = 3;
+
+        int c = a + b;
+        int d = a - b;
+
+        int e = a * b;
+        int f = a / b;
+
+        int g = a % b;
+
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+        System.out.println("f = " + f);
+        System.out.println("g = " + g);
+    }
+
+
+}
+
