48b816916ba488ab6b1d989a4aea7ecec42e5c80
Oleg <o.selivancev@gmail.com>
2019-03-10 03:13:41 +0300

    JavaLessonUpdate

--- HelloWorld/src/com/javalesson/operators/Operators.java
+++ HelloWorld/src/com/javalesson/operators/Operators.java
@@ -4,22 +4,43 @@ public class Operators {
 
     public static void main(String[] args) {
 //          + - / *  % - остаток от деления
-        int a = 10;
-        int b = 3;
+        double a = 10;
+        double b = 3;
 
-        int c = a + b;
-        int d = a - b;
+        double c = a + b;
+        double d = a - b;
 
-        int e = a * b;
-        int f = a / b;
+        double e = a * b;
+        double f = a / b;
 
-        int g = a % b;
+        double g = a % b;
 
         System.out.println("c = " + c);
         System.out.println("d = " + d);
         System.out.println("e = " + e);
         System.out.println("f = " + f);
         System.out.println("g = " + g);
+
+        //incriment DEcriment
+        //a = a + 5;
+        a += 5;
+        a++; // инкриментирование
+
+        System.out.println("New a = " + a);
+
+
+        --b;
+        System.out.println("New b = " + b);
+
+        double n = 7;
+        double m = 7;
+
+        double res1 = 2 * n++; //postFix-ная операция
+        double res2 = 2 * ++m; //preFix-ная операция
+
+        System.out.println("res1 = " + res1);
+        System.out.println("res2 = " + res2);
+
     }
 
 
